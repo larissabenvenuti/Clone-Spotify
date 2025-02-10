fab22a616544f129eb56ad2cadeb5e0f71c85bd2
Larissa <larissabenvenutia@gmail.com>
2025-02-10 19:49:23 -0300

    Commit final

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 # Clone-Spotify
-Clone do Spotify para fins didáticos.
-=======
-# spotify-imersao
->>>>>>> 6ce6fb5 (Clone do Spotify - inicio)
+#### Clone do Spotify para fins didáticos.
--- script.js
+++ script.js
@@ -13,10 +13,9 @@ greetingElement.textContent = greetingMessage;
 
 const container = document.querySelector(".offer__list-item");
 
-const observer = new ResizeObserver(() => {  
-  const containerWidth = container.offsetWidth; 
-  const numColumns = Math.floor(containerWidth / 200); 
-
+const observer = new ResizeObserver(() => {
+  const containerWidth = container.offsetWidth;
+  const numColumns = Math.floor(containerWidth / 200);
   container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
 
   console.log({ container });
